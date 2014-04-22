1cf6c2f47fe2b2f86d6217ae14c0df8acc63013f
Jeremy.Park <bum3144@gmail.com>
2014-04-22 20:08:34 +0900

    .

--- repositoryTest/src/servlets/subject/SubjectListServlet.java
+++ repositoryTest/src/servlets/subject/SubjectListServlet.java
@@ -18,6 +18,7 @@ import dao.SubjectDao;
  * - MVC 구조의 완성!
  */
 
+
 @WebServlet("/subject/list.bit")
 @SuppressWarnings("serial")
 public class SubjectListServlet extends HttpServlet {
@@ -34,18 +35,18 @@ public class SubjectListServlet extends HttpServlet {
 			}
 			*/
 			
-			SubjectDao dao = (SubjectDao)this.getServletContext()
-																							.getAttribute("subjectDao");
+
+			SubjectDao dao = (SubjectDao)this.getServletContext().getAttribute("subjectDao");			
+
 			
+
 			int pageNo = Integer.parseInt(request.getParameter("pageNo")); 
-			int pageSize = Integer.parseInt(request.getParameter("pageSize")); 
-			
+			int pageSize = Integer.parseInt(request.getParameter("pageSize"));			
 			List<SubjectVo> list = dao.list(pageNo, pageSize);
 			
 			// ServletRequest 보관소에 DAO 리턴 결과를 보관한다.
 			// JSP가 사용할 수 있도록 
-			request.setAttribute("list", list);
-			
+			request.setAttribute("list", list);			
 			// JSP에 실행을 위함하기 => RequestDispatcher를 얻는다.
 			// - 파라미터는 반드시 현재 컨텍스트(웹 애플리케이션 루트)를 기준으로 할 것.
 			RequestDispatcher rd = 
